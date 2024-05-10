e1bd6e5437a83a0184f9c53414a3345944162701
zwh <1716142352@qq.com>
2024-05-10 17:57:34 +0800

    feat:修改App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <h1>测试cicd</h1>
+
   <HelloWorld msg="Vite + Vue" />
 </template>
 
