b26bc0a0a693f84d94773905dbcaa8615c376da3
zwh <1716142352@qq.com>
2024-05-12 00:11:21 +0800

    feat:修改App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,39 @@
-<script setup lang="ts">
+<script setup>
 import HelloWorld from './components/HelloWorld.vue'
-const getClick = ()=>{
-  fetch('https://api.roamrover.site/').then(res=>{
-    console.log(res)
-  })
-}
 
+
+const fileUpload = (e) => {
+  let result = fetch('https://api.roamrover.site/put').then(res => res.json()).then(res => {
+    let file = e.target.files[0]
+    const formData = new FormData()
+    formData.append('key', file.name)
+    formData.append('policy', res.policy)
+    formData.append('OSSAccessKeyId', res.OSSAccessKeyId)
+    formData.append('success_action_status', 200)
+    formData.append('signature', res.signature)
+    formData.append('file', file)
+    fetch(res.host, {
+      method: 'POST',
+      body: formData
+    })
+  })
+
+
+}
 </script>
 
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/vite.svg" class="logo" alt="Vite logo"/>
     </a>
     <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo"/>
     </a>
   </div>
   <p>cicd</p>
-  <button @click="getClick">获取接口</button>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue"/>
+  <input type="file" @change="fileUpload">
 </template>
 
 <style scoped>
@@ -29,9 +43,11 @@ const getClick = ()=>{
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
